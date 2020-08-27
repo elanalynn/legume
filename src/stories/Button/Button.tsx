d131ec4e2aa2adf12082bb01e05ac12dd4de3355
Elana Kopelevich <ekopelevich@gmail.com>
2020-08-27 10:45:34 -0600

    converts styles to scss and organizes stories into folders

--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './button.css';
+import './button.scss';
 
 export interface ButtonProps {
   /**
@@ -34,11 +34,15 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   ...props
 }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       style={{ backgroundColor }}
       {...props}
     >
